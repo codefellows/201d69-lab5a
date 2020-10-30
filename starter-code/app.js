76d0d67a8b7977a4ecb80fd0157439690db0d020
James Gerstenberger <gerstej9@gmail.com>
2020-10-30 14:32:36 -0700

    add another parameter to functions  one and two, sum and multipy, and created code for function from question three

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,14 +8,14 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-  var add = a + b;
+function sum(a, b, c) { //eslint-disable-line
+  var add = a + b + c;
   var string = 'The sum of ' + a + ' and ' + b + ' is ' + add + '.';
   return [add, string];
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+//testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -28,14 +28,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-  var multiple = a*b;
+function multiply(a, b, c) { //eslint-disable-line
+  var multiple = a* b * c;
   var string = 'The product of ' + a + ' and ' + b + ' is ' + multiple +'.';
   return [multiple, string];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+//testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,13 +50,23 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
+//#####NOTES: Needed to change first two question functions, sum and multiply, to have three parameters 
+
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var add = sum(a, b, c)[0];
+  //console.log(add);
+  var multiple = multiply(a, b, c)[0];
+  //console.log(multiple);
+  var stringSum = a +' and ' + b + ' and ' + c + ' sum to ' + add + '.';
+  //console.log(stringSum);
+  var stringMultiple = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiple + '.';
+  //console.log(stringMultiple);
+  return[add, multiple, stringSum, stringMultiple];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
